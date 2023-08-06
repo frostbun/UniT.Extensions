90629002e4900c043fb18013cffa18253e7072f3
Manh Nguyen <manhnb1911@gmail.com>
2023-08-07 00:41:17 +0700

    move initializable, factory to extensions

---
+++ Scripts/UniTask/IAsyncInitializable.cs
@@ -0,0 +1,11 @@
+﻿#if UNIT_EXTENSIONS_UNITASK
+namespace UniT.Extensions
+{
+    using Cysharp.Threading.Tasks;
+
+    public interface IAsyncInitializable
+    {
+        public UniTask InitializeAsync();
+    }
+}
+#endif
