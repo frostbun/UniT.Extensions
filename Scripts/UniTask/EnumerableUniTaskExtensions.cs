7a12db6208a6891d0887ec3e404812820ae5be0b
Manh Nguyen <manhnb1911@gmail.com>
2023-08-25 14:21:11 +0700

    add async ForEach

---
+++ Scripts/UniTask/EnumerableUniTaskExtensions.cs
@@ -0,0 +1,17 @@
+namespace UniT.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using Cysharp.Threading.Tasks;
+
+    public static class EnumerableUniTaskExtensions
+    {
+        public static async UniTask ForEach<T>(this IEnumerable<T> enumerable, Func<T, UniTask> action)
+        {
+            foreach (var item in enumerable)
+            {
+                await action(item);
+            }
+        }
+    }
+}
