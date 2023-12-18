1254f930f6448a9cf1fc7220d497e8ac324a955a
Manh Nguyen <manhnb1911@gmail.com>
2023-12-18 15:06:37 +0700

    add SerializableDictionary

---
+++ Scripts/Utilities/SerializableDictionary.cs
@@ -0,0 +1,37 @@
+﻿namespace UniT.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEngine;
+
+    [Serializable]
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+    {
+        [SerializeField] private KeyValuePair[] keyValuePairs;
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+            this.keyValuePairs = this.Select((key, value) => new KeyValuePair(key, value)).ToArray();
+        }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            this.Clear();
+            this.keyValuePairs.ForEach(pair => this.Add(pair.Key, pair.Value));
+        }
+
+        [Serializable]
+        private class KeyValuePair
+        {
+            [field: SerializeField] public TKey   Key   { get; private set; }
+            [field: SerializeField] public TValue Value { get; private set; }
+
+            public KeyValuePair(TKey key, TValue value)
+            {
+                this.Key   = key;
+                this.Value = value;
+            }
+        }
+    }
+}
