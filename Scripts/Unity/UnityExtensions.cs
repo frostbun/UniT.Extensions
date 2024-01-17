ff0cecae0cbfd14a017e50aa4310fd03d6223734
Manh Nguyen <manhnb1911@gmail.com>
2024-01-17 09:57:13 +0700

    Object.Instantiate,Object.Destroy extension

--- Scripts/Unity/UnityExtensions.cs
+++ Scripts/Unity/UnityExtensions.cs
@@ -4,10 +4,20 @@ namespace UniT.Extensions
 
     public static class UnityExtensions
     {
+        public static T Instantiate<T>(this T prefab, Vector3 position = default, Quaternion rotation = default, Transform parent = null) where T : Object
+        {
+            return Object.Instantiate(prefab, position, rotation, parent);
+        }
+
         public static T DontDestroyOnLoad<T>(this T obj) where T : Object
         {
             Object.DontDestroyOnLoad(obj);
             return obj;
+        }
+
+        public static void Destroy(this Object obj)
+        {
+            Object.Destroy(obj);
         }
 
         public static Sprite CreateSprite(this Texture2D texture, Vector2? pivot = null)
