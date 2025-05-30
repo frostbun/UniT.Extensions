3b1ca84d1fddb412948edaac47695c6f1bbfb8e6
Manh Nguyen <manhnb1911@gmail.com>
2025-05-30 11:09:36 +0700

    1.0.82

--- Scripts/Utilities/Operator.cs
+++ Scripts/Utilities/Operator.cs
@@ -1,6 +1,8 @@
 ﻿#nullable enable
 namespace UniT.Extensions
 {
+    using UnityEngine;
+
     public static class Operator
     {
         #region int
@@ -174,6 +176,96 @@ namespace UniT.Extensions
         public static bool Or(bool a, bool b) => a | b;
 
         public static bool Xor(bool a, bool b) => a ^ b;
+
+        #endregion
+
+        #region Vector2
+
+        public static Vector2 Add(Vector2 a, Vector2 b) => a + b;
+
+        public static Vector2 Subtract(Vector2 a, Vector2 b) => a - b;
+
+        public static Vector2 Multiply(Vector2 a, Vector2 b) => Vector2.Scale(a, b);
+
+        public static Vector2 Divide(Vector2 a, Vector2 b) => new Vector2(a.x / b.x, a.y / b.y);
+
+        public static Vector2 Min(Vector2 a, Vector2 b) => Vector2.Min(a, b);
+
+        public static Vector2 Max(Vector2 a, Vector2 b) => Vector2.Max(a, b);
+
+        public static Vector2 Difference(Vector2 a, Vector2 b) => new Vector2(Mathf.Abs(a.x - b.x), Mathf.Abs(a.y - b.y));
+
+        #endregion
+
+        #region Vector3
+
+        public static Vector3 Add(Vector3 a, Vector3 b) => a + b;
+
+        public static Vector3 Subtract(Vector3 a, Vector3 b) => a - b;
+
+        public static Vector3 Multiply(Vector3 a, Vector3 b) => Vector3.Scale(a, b);
+
+        public static Vector3 Divide(Vector3 a, Vector3 b) => new Vector3(a.x / b.x, a.y / b.y, a.z / b.z);
+
+        public static Vector3 Min(Vector3 a, Vector3 b) => Vector3.Min(a, b);
+
+        public static Vector3 Max(Vector3 a, Vector3 b) => Vector3.Max(a, b);
+
+        public static Vector3 Difference(Vector3 a, Vector3 b) => new Vector3(Mathf.Abs(a.x - b.x), Mathf.Abs(a.y - b.y), Mathf.Abs(a.z - b.z));
+
+        #endregion
+
+        #region Vector4
+
+        public static Vector4 Add(Vector4 a, Vector4 b) => a + b;
+
+        public static Vector4 Subtract(Vector4 a, Vector4 b) => a - b;
+
+        public static Vector4 Multiply(Vector4 a, Vector4 b) => Vector4.Scale(a, b);
+
+        public static Vector4 Divide(Vector4 a, Vector4 b) => new Vector4(a.x / b.x, a.y / b.y, a.z / b.z);
+
+        public static Vector4 Min(Vector4 a, Vector4 b) => Vector4.Min(a, b);
+
+        public static Vector4 Max(Vector4 a, Vector4 b) => Vector4.Max(a, b);
+
+        public static Vector4 Difference(Vector4 a, Vector4 b) => new Vector4(Mathf.Abs(a.x - b.x), Mathf.Abs(a.y - b.y), Mathf.Abs(a.z - b.z), Mathf.Abs(a.w - b.w));
+
+        #endregion
+
+        #region Vector2Int
+
+        public static Vector2Int Add(Vector2Int a, Vector2Int b) => a + b;
+
+        public static Vector2Int Subtract(Vector2Int a, Vector2Int b) => a - b;
+
+        public static Vector2Int Multiply(Vector2Int a, Vector2Int b) => Vector2Int.Scale(a, b);
+
+        public static Vector2Int Divide(Vector2Int a, Vector2Int b) => new Vector2Int(a.x / b.x, a.y / b.y);
+
+        public static Vector2Int Min(Vector2Int a, Vector2Int b) => Vector2Int.Min(a, b);
+
+        public static Vector2Int Max(Vector2Int a, Vector2Int b) => Vector2Int.Max(a, b);
+
+        public static Vector2Int Difference(Vector2Int a, Vector2Int b) => new Vector2Int(Mathf.Abs(a.x - b.x), Mathf.Abs(a.y - b.y));
+
+        #endregion
+
+        #region Vector3Int
+
+        public static Vector3Int Add(Vector3Int a, Vector3Int b) => a + b;
+
+        public static Vector3Int Subtract(Vector3Int a, Vector3Int b) => a - b;
+
+        public static Vector3Int Multiply(Vector3Int a, Vector3Int b) => Vector3Int.Scale(a, b);
+
+        public static Vector3Int Divide(Vector3Int a, Vector3Int b) => new Vector3Int(a.x / b.x, a.y / b.y);
+
+        public static Vector3Int Min(Vector3Int a, Vector3Int b) => Vector3Int.Min(a, b);
+
+        public static Vector3Int Max(Vector3Int a, Vector3Int b) => Vector3Int.Max(a, b);
+
+        public static Vector3Int Difference(Vector3Int a, Vector3Int b) => new Vector3Int(Mathf.Abs(a.x - b.x), Mathf.Abs(a.y - b.y));
 
         #endregion
     }
