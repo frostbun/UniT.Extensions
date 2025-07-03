caeb4d4f7d745813b7dd1dbb7872bac669b55663
Manh Nguyen <manhnb1911@gmail.com>
2025-07-03 15:19:18 +0700

    1.0.85

--- Scripts/Unity/ColorExtensions.cs
+++ Scripts/Unity/ColorExtensions.cs
@@ -6,14 +6,29 @@ namespace UniT.Extensions
 
     public static class ColorExtensions
     {
+        public static void SetColor(this SpriteRenderer spriteRenderer, Color color)
+        {
+            spriteRenderer.color = color;
+        }
+
         public static void SetAlpha(this SpriteRenderer spriteRenderer, float a)
         {
             spriteRenderer.color = spriteRenderer.color.WithAlpha(a);
         }
 
+        public static void SetColor(this Material material, Color color)
+        {
+            material.color = color;
+        }
+
         public static void SetAlpha(this Material material, float a)
         {
             material.color = material.color.WithAlpha(a);
+        }
+
+        public static void SetColor(this Graphic graphic, Color color)
+        {
+            graphic.color = color;
         }
 
         public static void SetAlpha(this Graphic graphic, float a)
