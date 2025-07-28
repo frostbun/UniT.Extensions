9b4a83cf7b8f45e957bcee0b6261310e84b4ff62
Manh Nguyen <manhnb1911@gmail.com>
2025-07-28 19:36:40 +0700

    1.0.87

---
+++ Scripts/Serializables/SerializableReadOnlyDictionary.cs
@@ -0,0 +1,45 @@
+﻿#if ODIN_INSPECTOR
+#nullable enable
+namespace UniT.Extensions
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    #if UNITY_EDITOR
+    using Sirenix.OdinInspector.Editor;
+    #endif
+
+    [Serializable]
+    public class SerializableReadOnlyDictionary<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>
+    {
+        [SerializeField] private SerializableDictionary<TKey, TValue> values = new SerializableDictionary<TKey, TValue>();
+
+        public int Count => this.values.Count;
+
+        public TValue this[TKey key] => this.values[key];
+
+        public IEnumerable<TKey> Keys => this.values.Keys;
+
+        public IEnumerable<TValue> Values => this.values.Values;
+
+        public bool ContainsKey(TKey key) => this.values.ContainsKey(key);
+
+        public bool TryGetValue(TKey key, out TValue value) => this.values.TryGetValue(key, out value);
+
+        IEnumerator IEnumerable.GetEnumerator() => this.values.GetEnumerator();
+
+        IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator() => this.values.GetEnumerator();
+    }
+
+    #if UNITY_EDITOR
+    internal sealed class SerializableReadOnlyDictionaryDrawer<TKey, TValue> : OdinValueDrawer<SerializableReadOnlyDictionary<TKey, TValue>>
+    {
+        protected override void DrawPropertyLayout(GUIContent label)
+        {
+            this.Property.Children["values"].Draw(label);
+        }
+    }
+    #endif
+}
+#endif
---
+++ Scripts/Serializables/SerializableReadOnlyList.cs
@@ -0,0 +1,55 @@
+﻿#nullable enable
+namespace UniT.Extensions
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    #if UNITY_EDITOR
+    #if ODIN_INSPECTOR
+    using Sirenix.OdinInspector.Editor;
+    #else
+    using UnityEditor;
+    #endif
+    #endif
+
+    [Serializable]
+    public class SerializableReadOnlyList<T> : IReadOnlyList<T>
+    {
+        [SerializeReference] private List<T> values = new List<T>();
+
+        public int Count => this.values.Count;
+
+        public T this[int index] => this.values[index];
+
+        IEnumerator IEnumerable.GetEnumerator() => this.values.GetEnumerator();
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator() => this.values.GetEnumerator();
+    }
+
+    #if UNITY_EDITOR
+    #if ODIN_INSPECTOR
+    internal sealed class SerializableReadOnlyListDrawer<T> : OdinValueDrawer<SerializableReadOnlyList<T>>
+    {
+        protected override void DrawPropertyLayout(GUIContent label)
+        {
+            this.Property.Children["values"].Draw(label);
+        }
+    }
+    #else
+    [CustomPropertyDrawer(typeof(SerializableReadOnlyList<>), useForChildren: true)]
+    internal sealed class SerializableReadOnlyListDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("values"), label, includeChildren: true);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property.FindPropertyRelative("values"), label, includeChildren: true);
+        }
+    }
+    #endif
+    #endif
+}
