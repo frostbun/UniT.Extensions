37a5bebcb01f28ad0e73ce7d0bb2be55c3d2a6aa
Manh Nguyen <manhnb1911@gmail.com>
2025-11-20 11:08:24 +0700

    feat: vector Clamp & IsInBounds

--- Scripts/Unity/VectorExtensions.cs
+++ Scripts/Unity/VectorExtensions.cs
@@ -88,5 +88,92 @@ namespace UniT.Extensions
             vector.z = z;
             return vector;
         }
+
+        public static Vector2 Clamp(this Vector2 vector, Vector2 min, Vector2 max)
+        {
+            vector.x = Mathf.Clamp(vector.x, min.x, max.x);
+            vector.y = Mathf.Clamp(vector.y, min.y, max.y);
+            return vector;
+        }
+
+        public static Vector3 Clamp(this Vector3 vector, Vector3 min, Vector3 max)
+        {
+            vector.x = Mathf.Clamp(vector.x, min.x, max.x);
+            vector.y = Mathf.Clamp(vector.y, min.y, max.y);
+            vector.z = Mathf.Clamp(vector.z, min.z, max.z);
+            return vector;
+        }
+
+        public static Vector4 Clamp(this Vector4 vector, Vector4 min, Vector4 max)
+        {
+            vector.x = Mathf.Clamp(vector.x, min.x, max.x);
+            vector.y = Mathf.Clamp(vector.y, min.y, max.y);
+            vector.z = Mathf.Clamp(vector.z, min.z, max.z);
+            vector.w = Mathf.Clamp(vector.w, min.w, max.w);
+            return vector;
+        }
+
+        public static Vector2Int Clamp(this Vector2Int vector, Vector2Int min, Vector2Int max)
+        {
+            vector.x = Mathf.Clamp(vector.x, min.x, max.x);
+            vector.y = Mathf.Clamp(vector.y, min.y, max.y);
+            return vector;
+        }
+
+        public static Vector3Int Clamp(this Vector3Int vector, Vector3Int min, Vector3Int max)
+        {
+            vector.x = Mathf.Clamp(vector.x, min.x, max.x);
+            vector.y = Mathf.Clamp(vector.y, min.y, max.y);
+            vector.z = Mathf.Clamp(vector.z, min.z, max.z);
+            return vector;
+        }
+
+        public static bool IsInBounds(this Vector2 vector, Vector2 size)
+        {
+            return vector.x >= 0
+                && vector.x < size.x
+                && vector.y >= 0
+                && vector.y < size.y;
+        }
+
+        public static bool IsInBounds(this Vector3 vector, Vector3 size)
+        {
+            return vector.x >= 0
+                && vector.x < size.x
+                && vector.y >= 0
+                && vector.y < size.y
+                && vector.z >= 0
+                && vector.z < size.z;
+        }
+
+        public static bool IsInBounds(this Vector4 vector, Vector4 size)
+        {
+            return vector.x >= 0
+                && vector.x < size.x
+                && vector.y >= 0
+                && vector.y < size.y
+                && vector.z >= 0
+                && vector.z < size.z
+                && vector.w >= 0
+                && vector.w < size.w;
+        }
+
+        public static bool IsInBounds(this Vector2Int vector, Vector2Int size)
+        {
+            return vector.x >= 0
+                && vector.x < size.x
+                && vector.y >= 0
+                && vector.y < size.y;
+        }
+
+        public static bool IsInBounds(this Vector3Int vector, Vector3Int size)
+        {
+            return vector.x >= 0
+                && vector.x < size.x
+                && vector.y >= 0
+                && vector.y < size.y
+                && vector.z >= 0
+                && vector.z < size.z;
+        }
     }
 }
